8dbfdcce06a31efe9f5bdec247de845e088759b8
Valencianos <malina_mami@mail.ru>
2024-12-02 22:44:02 +0300

    add logic to store

---
+++ src/api/paintings.js
@@ -0,0 +1,44 @@
+const _paintings = [
+  {
+    id: 1,
+    title: "«Рождение Венеры» Сандро Боттичелли",
+    imgUrl: "boticcelli.png",
+    oldPrice: 2000000,
+    price: 1000000,
+    isSold: false,
+    qty: 1,
+  },
+  {
+    id: 2,
+    title: "«Тайная вечеря»  Леонардо да Винчи",
+    imgUrl: "leonardo.png",
+    oldPrice: 0,
+    price: 3000000,
+    isSold: false,
+    qty: 1,
+  },
+  {
+    id: 3,
+    title: "«Сотворение Адама» Микеланджело",
+    imgUrl: "mickelangelo.png",
+    oldPrice: 6000000,
+    price: 5000000,
+    isSold: false,
+    qty: 1,
+  },
+  {
+    id: 4,
+    title: "«Урок анатомии»  Рембрандт",
+    imgUrl: "rembrandt.png",
+    oldPrice: 4000000,
+    price: 3000000,
+    isSold: true,
+    qty: 0,
+  },
+];
+
+export default {
+  getPaintings(cb) {
+    setTimeout(() => cb(_paintings), 1000);
+  },
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,48 +1,40 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import paintings from "@/api/paintings";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     links: ["Каталог", "Доставка", "Оплата", "Контакты", "О компании"],
-    paintings: [
-      {
-        id: 1,
-        title: "«Рождение Венеры» Сандро Боттичелли",
-        imgUrl: "boticcelli.png",
-        oldPrice: 2000000,
-        price: 1000000,
-        isSold: false,
-      },
-      {
-        id: 2,
-        title: "«Тайная вечеря»  Леонардо да Винчи",
-        imgUrl: "leonardo.png",
-        oldPrice: 0,
-        price: 3000000,
-        isSold: false,
-      },
-      {
-        id: 3,
-        title: "«Сотворение Адама» Микеланджело",
-        imgUrl: "mickelangelo.png",
-        oldPrice: 6000000,
-        price: 5000000,
-        isSold: false,
-      },
-      {
-        id: 4,
-        title: "«Урок анатомии»  Рембрандт",
-        imgUrl: "rembrandt.png",
-        oldPrice: 4000000,
-        price: 3000000,
-        isSold: true,
-      },
-    ],
+    paintings: [],
   },
-  getters: {},
-  mutations: {},
-  actions: {},
+  getters: {
+    availablePaintings(state) {
+      return state.paintings.filter((painting) => painting.qty > 0);
+    },
+  },
+  mutations: {
+    setPaintings(state, paintings) {
+      state.paintings = paintings;
+    },
+  },
+  actions: {
+    fetchPaintings({ commit }) {
+      return new Promise((resolve) => {
+        paintings.getPaintings((paintings) => {
+          commit("setPaintings", paintings);
+          resolve();
+        });
+      });
+    },
+    addToCart(context, painting) {
+      if (painting.qty > 0) {
+        context.commit("pushProductToCart", painting);
+      } else {
+        // something happening
+      }
+    },
+  },
   modules: {},
 });
