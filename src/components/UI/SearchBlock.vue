6f9e511f1e1e126fef0df0d95920400babdee268
StanMalinovskii <malina_mami@mail.ru>
2024-12-03 12:02:33 +0300

    new changes

--- src/components/UI/SearchBlock.vue
+++ src/components/UI/SearchBlock.vue
@@ -1,11 +1,45 @@
 <script>
+import ButtonUI from "@/components/UI/ButtonUI.vue";
+
 export default {
   name: "SearchBlock",
+  components: { ButtonUI },
+  computed: {
+    filteredPaintings() {
+      return this.$store.state.paintings.filter((pic) => {
+        return (
+          pic.title.toLowerCase().indexOf(this.search.toLowerCase()) !== -1
+        );
+      });
+    },
+  },
 };
 </script>
 
 <template>
-  <div></div>
+  <form>
+    <div class="form-wrapper">
+      <input
+        type="text"
+        name="search"
+        id="search-input"
+        placeholder="Поиск по названию картины"
+      />
+      <label for="search-input"></label>
+      <ButtonUI btnTitle="Найти" @click="filteredPaintings" />
+    </div>
+  </form>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@use "@/assets/styles/_vars.scss" as *;
+.form-wrapper {
+  display: flex;
+  border: 1px solid $colorBorderInactive;
+  align-items: center;
+}
+
+#search-input {
+  width: 416px;
+}
+</style>
